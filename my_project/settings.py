8005b5a56b4c019bcdf7e14b238bba1da27405d9
v-amna <amnamampad@gmail.com>
2026-04-14 14:00:40 +0200

    Add Procfile, update settings for production, and modify requirements

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dekl&ci_o-6u58m@9!pga6x-3c6k!xyrx!-0tamf#g%6x_4io6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-nielmc-django-project-0kylrta3cs.us2.codeanyapp.com',]
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1',]
 
 
 # Application definition
